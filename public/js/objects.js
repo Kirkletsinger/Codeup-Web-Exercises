2ebbedf77f8be683d804decf0906c2f16d8d8ac0
kirk <kirkletgo@gmail.com>
2016-08-03 15:24:33 -0500

    Added objects

---
+++ public/js/objects.js
@@ -0,0 +1,19 @@
+(function(){
+    "use strict";
+    // TODO: Create person object
+    // var person = todo;
+    var person = {}     
+    // TODO: Create firstName and lastName properties in your person object; assign your name to them
+        person.firstName = 'Kirk';
+        person.lastName = 'Letsinger';
+    // TODO: Add a sayHello method to the person object that
+    //       alerts a greeting using the firstName and lastName properties
+        person.sayHello = function(){
+        alert("hello" + person.firstName + " " + person.lastName);
+    };
+    // Say hello from the person object.
+     person.sayHello();
+})();
+
+
+
