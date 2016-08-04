7a3415b9e47ba95a91161c9e6484779cf74c26f4
kirk <kirkletgo@gmail.com>
2016-08-04 16:38:26 -0500

    Defue-the-bom, circle , math-js, and extra objects exercises added

---
+++ public/js/lecture.js
@@ -0,0 +1,163 @@
+'use strict';
+
+////// 8/3/16 split join/////
+
+var myString = 'codeup';
+var stringAsArray = myString.split('');
+var arrayReversed =stringAsArray.reverse();
+var revesedString = arrayReversed.join('');
+
+///sum the digits in a number 
+//turn my number into a string 
+//turn that sting into an array of digits
+//loop through that array and add together the digits
+
+function sumTheDigits(){
+	var numberString = number.toString();
+	var numberArray = numberString.split('');
+
+}
+
+
+
+///// 8/3/16 //////
+
+// What mrhods change the original variable?
+
+//foreach loops - 5ws + h
+//foreach stuff inside for each continue dosen't work! what goes inside a foreach funtion?
+
+var names = ['zach' , 'will' , 'fernando']
+
+names.f
+
+
+
+
+
+
+
+
+
+
+///string stuff together -- nested properties in objects
+//josn -- syntax, what is vaild json? best practices
+
+
+//splicing 
+array.slpice(starIndex, numberOfItemsToRemove, itemTOInsert1, itemTOInsert2, ..)
+//remove 2 elements
+var index = array.indexOf(somthing);
+//insert 2 elements
+array.splice(index, 0, item1, item2);
+
+
+//js -- if else tying everything together -- what good are all these books if we're only console.log
+//multipages of js
+
+
+
+//scope of 'this' keyword
+//this 'keyword' why this instead of the variable name
+var me = {
+	name: 'zach',
+	saymyname: function() {
+		return this.name;
+	}
+}
+//when to use alert confirm, prompt
+//use models
+
+//playing computer
+
+
+
+
+//multiline string, number.max,min value
+
+var myString = 'hello\
+ world'; // => 'hello world'
+
+// or
+
+
+
+
+
+var myString = 'hello' +
+	myString'world';
+
+	
+//array literal suntax
+
+function returnThree(){
+	return 3;
+}
+
+[1, 1 + 1, returnThree(), returnThree( + 1)]
+
+//object literal syntax
+{
+	one: 1,
+	two: 1 + 1,
+	three: returnThree(),
+	four: returnThree() + 1
+}
+ {propery.name } 
+
+
+var a = 123;
+var b = a;
+
+console.log("a: " + a +"b:"); // a: 123 b:123
+
+
+
+
+
+//JSON: JavaScript Object Nothation -- json.org --for syntax
+//data interchange format of the modern web e.g. mysql, pg datatypes
+//reddit api, git hub api, pokemkn api,weather api
+//syntax
+//subset of objects we use in our js
+//indentation conveys nesting
+//values can only be literals
+//property names must be strings
+//all strinds must be double-quoted
+//no trailing commas
+//no comments
+
+
+
+
+student.cars[1].make
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
